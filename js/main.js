d840d109297b3ea0dc0168671764cc794eefeaac
Fernando Bueno <nando_silverz@hotmail.com>
2023-04-21 07:13:30 -0300

    Adicionado main.js

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const pom = document.querySelector(".tecla_pom");
+const clap = document.querySelector(".tecla_clap");
+const tim = document.querySelector(".tecla_tim");
+
+const puff = document.querySelector(".tecla_puff");
+const splash = document.querySelector(".tecla_splash");
+const toim = document.querySelector(".tecla_toim");
+
+const psh = document.querySelector(".tecla_psh");
+const tic = document.querySelector(".tecla_tic");
+const tom = document.querySelector(".tecla_tom");
+
+
