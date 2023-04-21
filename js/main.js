643c3bd0343a71eef4c8add2faf1616fde32e61a
Fernando Bueno <nando_silverz@hotmail.com>
2023-04-21 10:03:05 -0300

    Refatorado main.js

--- js/main.js
+++ js/main.js
@@ -1,50 +1,8 @@
-const pom = document.querySelector(".tecla_pom");
-const clap = document.querySelector(".tecla_clap");
-const tim = document.querySelector(".tecla_tim");
+const listaDeTeclas = document.querySelectorAll(".tecla");
+const listaDeSons = document.querySelectorAll(".som")
 
-const puff = document.querySelector(".tecla_puff");
-const splash = document.querySelector(".tecla_splash");
-const toim = document.querySelector(".tecla_toim");
-
-const psh = document.querySelector(".tecla_psh");
-const tic = document.querySelector(".tecla_tic");
-const tom = document.querySelector(".tecla_tom");
-
-pom.addEventListener("click", function () {
-    document.querySelector("#som_tecla_pom").play()
+listaDeTeclas.forEach((indiceTecla, indiceSom) => {
+    indiceTecla.addEventListener("click", function () {
+        listaDeSons[indiceSom].play();
+    });
 });
-
-clap.addEventListener("click", function () {
-    document.querySelector("#som_tecla_clap").play()
-});
-
-tim.addEventListener("click", function () {
-    document.querySelector("#som_tecla_tim").play()
-});
-
-puff.addEventListener("click", function () {
-    document.querySelector("#som_tecla_puff").play()
-});
-
-splash.addEventListener("click", function () {
-    document.querySelector("#som_tecla_splash").play()
-});
-
-toim.addEventListener("click", function () {
-    document.querySelector("#som_tecla_toim").play()
-});
-
-psh.addEventListener("click", function () {
-    document.querySelector("#som_tecla_psh").play()
-});
-
-tic.addEventListener("click", function () {
-    document.querySelector("#som_tecla_tic").play()
-});
-
-tom.addEventListener("click", function () {
-    document.querySelector("#som_tecla_tom").play()
-});
-
-
-
